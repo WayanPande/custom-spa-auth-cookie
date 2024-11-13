eb552015849f68d45a90e6ac27db58aab647e1fd
WayanPande <yande554@gmail.com>
2024-11-13 10:13:52 +0800

    fix: asset base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [TanStackRouterVite({}), react()],
-});
+// @ts-ignore
+export default ({ mode }) => {
+  // @ts-ignore
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [TanStackRouterVite({}), react()],
+    // @ts-ignore
+    base: `/${process.env.VITE_ASSET_PATH ?? ""}`,
+  });
+};
