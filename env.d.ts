e64926ecc71baaeee25c3b0c02657de00e926547
WayanPande <yande554@gmail.com>
2024-11-13 10:27:29 +0800

    fix: try fix router base path

--- env.d.ts
+++ env.d.ts
@@ -3,6 +3,7 @@ interface ImportMetaEnv {
   readonly VITE_API_URL: string;
   readonly VITE_COOKIE_NAME: string;
   readonly VITE_AUTH_SECRET: string;
+  readonly VITE_ASSET_PATH: string;
 }
 
 interface ImportMeta {
